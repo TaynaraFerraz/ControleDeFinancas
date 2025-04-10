cc183b337b69d91ee76b904d628d41054f792a87
TaynaraFerraz <taynaracferraz@gmail.com>
2025-04-10 19:02:26 -0300

    adicionando configuração dos cookies para acesso a informações privadas

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -2,8 +2,67 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middleware/check-session-is-exists'
 
 export async function transactionRoutes(app: FastifyInstance) {
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
+
+      return { transactions }
+    },
+  )
+
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getTransactionParamsSchema.parse(request.params)
+
+      const { sessionId } = request.cookies
+
+      const transactions = await knex('transactions')
+        .where({
+          id,
+          session_id: sessionId,
+        })
+        .first()
+
+      return { transactions }
+    },
+  )
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+
+      return { summary }
+    },
+  )
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -15,10 +74,22 @@ export async function transactionRoutes(app: FastifyInstance) {
       request.body,
     ) // validando os dados da minha requisição, para ver se eles batem com os dados do tipo createTransactionBodySchema. Se der erro, nada abaixo irá executar pois irá gerar um throw error
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/', // qualquer rota pode acessar os cookies
+        maxAge: 60 * 60 * 24 * 7, // 7 dias
+        sameSite: 'lax',
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     }) // para inserir não precisa passar o type ja que nao tem uma coluna reservada para ele
 
     return reply.status(201).send()
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env'
+import cookie from '@fastify/cookie'
 import { transactionRoutes } from './routes/transaction'
+
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactionRoutes, {
   prefix: 'transactions',
